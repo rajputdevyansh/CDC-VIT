47e0fcd29b23888528393c6b7c6c3f4f6b1d1636
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-25 09:50:42 +0530

    Update strongno.java

--- Hunts-Cliff/strongno.java
+++ Hunts-Cliff/strongno.java
@@ -1,1 +1,33 @@
+package Test7Hunts;
 
+import java.util.Scanner;
+class strongno
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        sc.close();
+        int r,n,fact=1;
+        int sum;
+        for(int i=a;i<=b;i++)
+        {
+            n=i;
+            sum=0;
+            while(n!=0)
+            {
+                r=n%10;
+                fact=1;
+                for(int k=1;k<=r;k++)
+                {
+                    fact=fact*k;
+                }
+                sum=sum+fact;
+                n=n/10;
+            }
+            if(sum==i)
+            System.out.print(i+" ");
+        }
+    }
+}
