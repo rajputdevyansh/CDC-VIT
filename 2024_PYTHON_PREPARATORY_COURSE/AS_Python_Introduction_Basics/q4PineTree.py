ad5c4d865b115f2eac05ea4afedb95022488e15d
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 19:05:20 +0530

    Update q4PineTree.py

--- 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q4PineTree.py
+++ 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q4PineTree.py
@@ -1,2 +1,6 @@
+"""
+Nikhil, the founder of "Pine Tree" company wished to design an Event Management System that would let its Customers plan and host events seamlessly via an online platform.
+As a part of this requirement, Nikhil wanted to write a piece of code for his company's Pineaxe Event Management System that will display customized welcome messages by taking Customers' name as input. Help Nikhil on the task.
+"""
 Name=str(input())
 print("Hello",Name,"! Welcome to  Pineaxe Event Management System")
