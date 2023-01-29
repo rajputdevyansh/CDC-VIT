f1494eeaec2b33984505e5926f872c8e1181d982
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 19:02:47 +0530

    Update q1pencils.py

--- 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q1pencils.py
+++ 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q1pencils.py
@@ -1,3 +1,6 @@
+"""
+Abu runs a shop near a school and hence sells a lot of pencils.Anbu wants to write a program to find the number of pencils sold everyday. The program will accept the number of pencil at N the beginning of the day and the number of pencils E at the end of the day(N>=E). The program must print the number of pencils sold on that particular day.
+"""
 N=int(input())
 E=int(input())
 print(N-E)
