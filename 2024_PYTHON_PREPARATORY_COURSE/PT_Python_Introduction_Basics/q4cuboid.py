6b42dba28c83b0e32d02b805bdab658274c09331
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 18:03:54 +0530

    Create q4cuboid.py

---
+++ 2024_PYTHON_PREPARATORY_COURSE/PT_Python_Introduction_Basics/q4cuboid.py
@@ -0,0 +1,7 @@
+"""
+The length L,breadth B and height H of a cuboid are passed as input and the program must print the volume of the cuboid as the output
+"""
+L=int(input())
+B=int(input())
+H=int(input())
+print(L*B*H)
