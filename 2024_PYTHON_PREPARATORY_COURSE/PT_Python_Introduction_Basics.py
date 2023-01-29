e4ad4a6a3099f4a2d202bd04be9f240520107fd3
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 17:59:48 +0530

    Delete PT_Python_Introduction_Basics.py

--- 2024_PYTHON_PREPARATORY_COURSE/PT_Python_Introduction_Basics.py
+++
@@ -1,5 +0,0 @@
-"""
-The price P of an item is passed as the input.The program must print the value of Pformatting it upto 2 places after decimal point. The output of the program must be "The price is p"
-"""
-p=float(input())
-print("The price is","%.2f"%p)
