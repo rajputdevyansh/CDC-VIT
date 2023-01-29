53632b081d4ceb1dd1584dd75820f7588a57a6b9
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 18:03:03 +0530

    Create q3seconds.py

---
+++ 2024_PYTHON_PREPARATORY_COURSE/PT_Python_Introduction_Basics/q3seconds.py
@@ -0,0 +1,8 @@
+"""
+A integer value H denoting number of hours is passed as the input. Also an integer value M denoting the minutes is passed as the input.The program must print the sum of the seconds present in H and M
+"""
+H=int(input())
+M=int(input())
+HS=H*60*60
+MS=M*60
+print(HS+MS)
