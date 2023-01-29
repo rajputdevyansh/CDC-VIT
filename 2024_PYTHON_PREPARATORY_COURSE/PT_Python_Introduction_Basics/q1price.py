8c7c06026e5d43511a9a7d0152f5bb115f0618c1
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 18:01:14 +0530

    Create q1price.py

---
+++ 2024_PYTHON_PREPARATORY_COURSE/PT_Python_Introduction_Basics/q1price.py
@@ -0,0 +1,5 @@
+"""
+The price P of an item is passed as the input.The program must print the value of Pformatting it upto 2 places after decimal point. The output of the program must be "The price is p"
+"""
+p=float(input())
+print("The price is","%.2f"%p)
