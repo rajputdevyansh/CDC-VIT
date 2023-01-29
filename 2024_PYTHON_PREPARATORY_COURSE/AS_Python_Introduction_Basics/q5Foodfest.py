9b7700cf559ec0d1af910f71d27cca4ae0cdf983
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 19:05:41 +0530

    Update q5Foodfest.py

--- 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q5Foodfest.py
+++ 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q5Foodfest.py
@@ -1,5 +1,7 @@
 """
-
+HillTown Inn is planning to organize a Food Festival bringing together at one place, a wide variety of cuisines from across the world on account of Christmas. The Hotel Management has rented out a square hall of an indoor Auditorium for this extravaganza.
+The side of the square hall is y inches in which a large square table is placed for the display of the most popular and celebrated food items. The side of the square table is x inches, such that x<y.
+The Management wanted to fill the remaining floor area with a decorative carpet. To get this done, they needed to know the floor area to be filled with the carpet. Write a program to help the Management find the area of the region located outside the square table, but inside the square hall.
 """
 a=int(input())
 b=int(input())
