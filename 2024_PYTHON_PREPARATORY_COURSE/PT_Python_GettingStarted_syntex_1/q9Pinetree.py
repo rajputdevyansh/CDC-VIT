0aeb6a4f791dcf474054ffe263133177ccf158ed
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 22:06:26 +0530

    Create q9Pinetree.py

---
+++ 2024_PYTHON_PREPARATORY_COURSE/PT_Python_GettingStarted_syntex_1/q9Pinetree.py
@@ -0,0 +1,6 @@
+"""
+"Pine Tree" Company has signed up a big time Event Management deal from the Rotary Youth Club for a Trade Fair organized at Codissia Complex, wherein all startup companies in the Software industry are demonstrating their latest products and services and meet with industry partners and Customers.
+Pineaxe Event Management System has to be modified to write a piece of code that will get the input of the number of events to be hosted for the Fair at Codissia from its users and display the same. Help the company to accomplish the requirement.
+"""
+E=int(input())
+print("Number of events hosted in Codissia is",E)
