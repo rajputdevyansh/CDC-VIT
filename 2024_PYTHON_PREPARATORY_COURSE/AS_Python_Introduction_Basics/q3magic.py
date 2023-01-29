9c55feaa3e502f85877441da8568e84586208b2b
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 19:04:42 +0530

    Update q3magic.py

--- 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q3magic.py
+++ 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Introduction_Basics/q3magic.py
@@ -1,3 +1,7 @@
+"""
+The Magic Castle, the home of the Academy of Magical Arts at California has organized the great 'WonderWorks Magic Show. 3 renowned magicians were invited to mystify and thrill the crowd with their world's spectacular magic tricks. At the end of each of the 3 magicians' shows, the audience were requested to give their feedback in a scale of 1 to
+10. Number of people who watched each show and the average feedback rating of each show is known. Write a program to find the average feedback rating of the WonderWorks Magic show.
+"""
 N1=int(input())
 R1=float(input())
 N2=int(input())
