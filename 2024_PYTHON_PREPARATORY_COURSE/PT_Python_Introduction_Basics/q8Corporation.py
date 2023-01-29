2a6c0336a375134d787d6c162246d5c49c8fa33f
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-29 18:07:08 +0530

    Create q8Corporation.py

---
+++ 2024_PYTHON_PREPARATORY_COURSE/PT_Python_Introduction_Basics/q8Corporation.py
@@ -0,0 +1,11 @@
+"""
+In ABC corporation,a program must accept the name of an employee,his age and his salary and print them as output with a line containing 2 asterisks separating them(refer sample input and output)
+"""
+Name=str(input())
+N=int(input())
+NF=float(input())
+print(Name)
+print("**")
+print(N)
+print("**")
+print(NF)
