e9aed8fd2700cc290e88f61563fc6d2095f0cd32
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2023-01-30 14:37:45 +0530

    Update q3length.py

--- 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Syntax_1/q3length.py
+++ 2024_PYTHON_PREPARATORY_COURSE/AS_Python_Syntax_1/q3length.py
@@ -1,5 +1,7 @@
 """
 
+Get input string from the user. Write a python program to calculate the length of the giver string.
+Note: Use len() function to calculate the length
 """
 Instr=str(input())
 print(len(Instr))
